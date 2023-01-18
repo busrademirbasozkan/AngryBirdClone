41e3182a2a2395c81f42f50ff066bfe31737cefa
busrademirbasozkan <busrademirbasozkan@gmail.com>
2023-01-18 23:36:04 +0300

    kutular için gamescene içinde tanımlama yapıldı.

--- AngryBirdClone/GameScene.swift
+++ AngryBirdClone/GameScene.swift
@@ -13,7 +13,11 @@ class GameScene: SKScene {
     //var bird2 = SKSpriteNode()
 
     var bird = SKSpriteNode()
-    
+    var box1 = SKSpriteNode()
+    var box2 = SKSpriteNode()
+    var box3 = SKSpriteNode()
+    var box4 = SKSpriteNode()
+    var box5 = SKSpriteNode()
     
     override func didMove(to view: SKView) {
         /* sıfırdan spritekitleri burada tanımlayabiliriz.
@@ -25,14 +29,58 @@ class GameScene: SKScene {
         bird2.zPosition = 1
         self.addChild(bird2)
     */
+        
+        //PhysicsBody
+        self.physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+        self.scene?.scaleMode = .aspectFit
+        
+        //bird
         bird = childNode(withName: "bird") as! SKSpriteNode
         let birdTexture = SKTexture(imageNamed: "bird")
         bird.physicsBody = SKPhysicsBody(circleOfRadius: birdTexture.size().height / 13)
         bird.physicsBody?.affectedByGravity = true
         bird.physicsBody?.isDynamic = true
         bird.physicsBody?.mass = 0.5
-        self.physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
         
+        
+        //box
+        let boxTexture = SKTexture(imageNamed: "brick")
+        let size = CGSize(width: boxTexture.size().width / 6, height: boxTexture.size().height / 6)
+        
+        box1 = childNode(withName: "box1") as! SKSpriteNode
+        box1.physicsBody = SKPhysicsBody(rectangleOf: size)
+        box1.physicsBody?.isDynamic = true
+        box1.physicsBody?.affectedByGravity = true
+        box1.physicsBody?.allowsRotation = true
+        box1.physicsBody?.mass = 0.4
+        
+        box2 = childNode(withName: "box2") as! SKSpriteNode
+        box2.physicsBody = SKPhysicsBody(rectangleOf: size)
+        box2.physicsBody?.isDynamic = true
+        box2.physicsBody?.affectedByGravity = true
+        box2.physicsBody?.allowsRotation = true
+        box2.physicsBody?.mass = 0.4
+        
+        box3 = childNode(withName: "box3") as! SKSpriteNode
+        box3.physicsBody = SKPhysicsBody(rectangleOf: size)
+        box3.physicsBody?.isDynamic = true
+        box3.physicsBody?.affectedByGravity = true
+        box3.physicsBody?.allowsRotation = true
+        box3.physicsBody?.mass = 0.4
+        
+        box4 = childNode(withName: "box4") as! SKSpriteNode
+        box4.physicsBody = SKPhysicsBody(rectangleOf: size)
+        box4.physicsBody?.isDynamic = true
+        box4.physicsBody?.affectedByGravity = true
+        box4.physicsBody?.allowsRotation = true
+        box4.physicsBody?.mass = 0.4
+        
+        box5 = childNode(withName: "box5") as! SKSpriteNode
+        box5.physicsBody = SKPhysicsBody(rectangleOf: size)
+        box5.physicsBody?.isDynamic = true
+        box5.physicsBody?.affectedByGravity = true
+        box5.physicsBody?.allowsRotation = true
+        box5.physicsBody?.mass = 0.4
         
     }
     
